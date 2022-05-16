b89782645e80403c83911c5d3cd6809bb9371b6e
李龙翔 <lilongxiang2000@gmail.com>
2022-05-16 17:57:44 +0800

    密码加密传输存储

--- bbs.js
+++ bbs.js
@@ -2,6 +2,7 @@ const express      = require('express')
 const cookieParser = require('cookie-parser')
 const fs           = require('fs')
 const svgCaptcha   = require('svg-captcha')
+const md5          = require('md5')
 
 
 const PORT     = 8080
@@ -19,7 +20,7 @@ function timeLocale() {
  * @param {*} str
  * @returns {String}
  */
- function escapeHTML(str) {
+function escapeHTML(str) {
   // 防止 sxx 攻击
   str = str.toString()
 
@@ -32,16 +33,6 @@ function timeLocale() {
   return str
 }
 
-/*
-  http://localhost:8080/
-  http://localhost:8080/post/post:id
-  http://localhost:8080/user/user:id
-
-  http://localhost:8080/login
-  http://localhost:8080/register
-  http://localhost:8080/forget
-
-*/
 
 app.use((req, res, next) => {
   console.log(req.method, req.url)
@@ -90,6 +81,18 @@ app.get('/', (req, res, next) => {
   next()
 })
 
+const sessionObjs = {}
+app.use(function sessionMW(req, res, next) {
+  if (!req.cookies.sessionID) {
+    let sessionID = Date.now()
+    res.cookie('sessionID', sessionID)
+    req.cookies.sessionID = sessionID
+  }
+
+  req.session = sessionObjs[req.cookies.sessionID] ?? (sessionObjs[req.cookies.sessionID] = {})
+
+  next()
+})
 
 // user
 app.get('/user/:username', (req, res, next) => {
@@ -125,8 +128,8 @@ app.post('/register', (req, res, next) => {
   // 防止传来 脏数据
   let userInfo = {
     username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
+    email   : req.body.email,
+    password: md5(req.body.password + md5(req.body.password.length)),
     isDelete: false,
     joinDate: Date.now()
   }
@@ -149,12 +152,10 @@ app.post('/register', (req, res, next) => {
 
 // login
 app.get('/login', (req, res, next) => {
-  let back = req.get('referer') ?? '/'
-
   if (req.signedCookies.loginUser) {
     res.redirect('/')
   } else res.type('html').render('login.pug', {
-    back: back
+    loginErrCount: req.session.loginErrCount
   })
 
   next()
@@ -163,24 +164,29 @@ app.post('/login', (req, res, next) => {
   // 防止传来 脏数据
   let loginInfo = {
     username: req.body.username,
-    password: req.body.password,
-    captcha: req.body.captcha == req.body.captchacText
+    password: md5(req.body.password + md5(req.body.password.length))
   }
 
-  let back = req.query.back ?? '/'
+  let captcha = req.body.captcha
+    ? req.session.captcha == req.body.captcha
+    : true
 
   let user = users.find(it =>
     it.username == loginInfo.username &&
     it.password == loginInfo.password
   )
 
-  if (user && loginInfo.captcha) {
+  if (user && captcha) {
     res.cookie('loginUser', loginInfo.username, {
       maxAge: 86400000, // 一天
       signed: true,
     })
-    res.redirect(back)
-  } else res.type('html').render('loginErr.pug')
+    req.session.loginErrCount = 0
+    res.redirect('/')
+  } else {
+    req.session.loginErrCount = (req.session.loginErrCount ?? 0) + 1
+    res.type('html').render('loginErr.pug', {captcha})
+  }
 
   next()
 })
@@ -190,9 +196,9 @@ app.get('/captcha', (req, res, next) => {
   let captcha = svgCaptcha.create({
     color: true,
     noise: 4,
-    ignoreChars: '0o1i'
+    ignoreChars: '0oOI1i'
   })
-  req.body.captchacText = captcha.text
+  req.session.captcha = captcha.text
   res.type('svg').end(captcha.data)
 
   next()
@@ -233,19 +239,19 @@ app.post('/post', (req, res, next) => {
   if (req.signedCookies.loginUser) {
     // 防止传来 脏数据
     let postInfo = {
-      id: `${Date.now()}`, // 待定
-      title: req.body.title,
-      text: req.body.text,
-      author: req.signedCookies.loginUser,
-      date: timeLocale(),
+      id      : `${Date.now()}`,               // 待定
+      title   : req.body.title,
+      text    : req.body.text,
+      author  : req.signedCookies.loginUser,
+      date    : timeLocale(),
       isDelete: false
     }
 
-    if (postInfo.title.length && postInfo.text.length) {
+    if (postInfo.title?.length && postInfo.text?.length) {
       posts.push(postInfo)
       fs.writeFileSync('./posts.json', JSON.stringify(posts, null, 2))
     }
-    res.redirect('/')
+    res.redirect(`/post/${postInfo.id}`)
   }
 
   next()
@@ -257,11 +263,11 @@ app.post('/comment/:postID', (req, res, next) => {
   if (req.signedCookies.loginUser) {
     // 防止传来 脏数据
     let commentInfo = {
-      id: `${Date.now()}`,
-      postID: req.params.postID,
-      text: req.body.text,
-      date: timeLocale(),
-      author: req.signedCookies.loginUser,
+      id      : `${Date.now()}`,
+      postID  : req.params.postID,
+      text    : req.body.text,
+      date    : timeLocale(),
+      author  : req.signedCookies.loginUser,
       isDelete: false
     }
     comments.push(commentInfo)
@@ -273,6 +279,6 @@ app.post('/comment/:postID', (req, res, next) => {
 })
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '127.0.0.1', () => {
   console.log('Listening on', PORT)
 })
